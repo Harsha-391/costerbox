3d555d1e24c0b313136517a70d779c1793f08525
Harsha-391 <143133227+Harsha-391@users.noreply.github.com>
2026-02-16 18:11:32 +0530

    dfgdg

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,11 +8,6 @@ import PromoStrip from '../components/PromoStrip';
 export const metadata = {
   title: 'Costerbox | Handcrafted Artistry',
   description: 'Celebrating Handcrafted Artistry & Timeless Design',
-  icons: {
-    icon: '/icon.png',
-    shortcut: '/icon.png',
-    apple: '/icon.png',
-  },
 };
 
 import { CartProvider } from '../context/CartContext';
